ad82121a8dba12005d5f775be3adc8266f974ace
BCihleln <33713630+BCihleln@users.noreply.github.com>
2025-01-27 23:39:35 +0800

    [Fix] Hide clinking object when ending
    Change Start() to OnEnable()

--- GGJ25-TeamI-BeerGame/Assets/Scripts/ClinkingControl.cs
+++ GGJ25-TeamI-BeerGame/Assets/Scripts/ClinkingControl.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class ClinkingControl : MonoBehaviour
 {
     public float selfDestoryTime = 2.0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    void OnEnable()
     {
         StartCoroutine(WaitTime());
     }
